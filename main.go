d4eec7773f17112e542e6e6230116a5051bd37a0
hanlsin <cool1022>
2017-03-01 14:33:24 -0500

    function call examples

--- main.go
+++ main.go
@@ -6,5 +6,16 @@ import "os"
 func main() {
 	fmt.Println("Hello Golang world!")
 
+	// Call function with a string argument.
+	hello("World!")
+
+	// Call funcion with a string argument, and check the return value.
+	fmt.Println("	Return = ", printFuncInfo("Hello World!"))
+	ret, _ := getFunctionInfo("Hello World?!?!?!?!?!?!")
+	fmt.Println(ret)
+
+	// Call function with function pointer.
+	exFunctionPointer()
+
 	os.Exit(0)
 }
